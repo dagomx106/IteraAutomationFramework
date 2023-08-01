0911f81ada8dfef53d14b93aa46a1008958e0c1d
dagomx106 <dagovf@outlook.com>
2023-08-01 08:02:46 -0600

    optimize imports

--- src/test/java/tests/TestVision.java
+++ src/test/java/tests/TestVision.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
